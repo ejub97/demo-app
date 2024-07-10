03f5baa4abbe8f03e94c207f17cda378d08f25e7
ejub hasanovic <ejubhasanovic@ejubs-MacBook-Pro.local>
2024-07-11 00:37:34 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// import { Carousel, initTWE } from "https://cdn.jsdelivr.net/npm/tw-elements@1.0.0-alpha12/dist/js/index.min.js";
-// initTWE({ Carousel });
+const menuBtn = document.querySelector(".header .menu-btn");
+const menu = document.querySelector(".header .menu-btn");
 
-// Function to load HTML content into a specific element
-function loadHTML(elementId, filePath) {
-    fetch(filePath)
-        .then(response => response.text())
-        .then(data => document.getElementById(elementId).innerHTML = data);
+function toggleMenu() {
+    menuBtn.classList.toggle("active");
+    menu.classList.toggle("open");
 }
 
-
-loadHTML('header', 'Components/header.html');
-loadHTML('hero', 'Components/hero.html');
-loadHTML('card', 'Components/card.html');
-// loadHTML('carousel', 'Components/carousel.html');
-loadHTML('footer', 'Components/footer.html');
+menuBtn.addEventListener("click", toggleMenu);
