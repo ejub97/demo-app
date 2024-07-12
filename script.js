71f8e70ce43f7a7464c1c17a1d5a818aa28e8472
ejub hasanovic <ejubhasanovic@ejubs-MacBook-Pro.local>
2024-07-13 01:04:22 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const menuBtn = document.querySelector(".header .menu-btn");
-const menu = document.querySelector(".header .menu-btn");
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector(".header .menu-btn");
+    const menu = document.querySelector(".header .menu");
 
-function toggleMenu() {
-    menuBtn.classList.toggle("active");
-    menu.classList.toggle("open");
-}
+    function toggleMenu() {
+        menuBtn.classList.toggle("active");
+        menu.classList.toggle("open");
+    }
 
-menuBtn.addEventListener("click", toggleMenu);
+    function closeMenu(event) {
+        if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
+            menuBtn.classList.remove("active");
+            menu.classList.remove("open");
+        }
+    }
+
+    menuBtn.addEventListener("click", toggleMenu);
+    document.addEventListener("click", closeMenu);
+});
